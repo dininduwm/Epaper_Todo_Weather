03779deb448edf0a125c7d5871bbffa977cebc32
dtdinidu7 <dtdinidu7@gmail.com>
2021-08-02 22:39:29 +0530

    error corrected

--- TSP_ePaper_Display.py
+++ TSP_ePaper_Display.py
@@ -1,7 +1,7 @@
 # The Signal Path - DataPad ePaper Display
 # Shahriar Shahramian / November 2018
 import os
-ibdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
+libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
 if os.path.exists(libdir):
     sys.path.append(libdir)
 
